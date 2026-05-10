53cf3433542b4985cdd0890e8dc36cc8418f82b9
antoinefa <fabien.antoine@m4x.org>
2026-05-10 10:23:14 -0400

    feat(core): add public types per Contract 1 (InputFile, TransposeInput, ...)

--- core/typescript/src/index.ts
+++ core/typescript/src/index.ts
@@ -1,1 +1,30 @@
 export const CORE_VERSION = '0.1.0-dev';
+
+// Public types
+export type {
+  CvMime,
+  InputFile,
+  BrandTokens,
+  Persistence,
+  TemplateAssets,
+  TransposeInput,
+  DetectedFields,
+  AlignmentReport,
+  TransposedCv,
+  TransposeOutput,
+} from './types.js';
+
+// LlmProvider (will be created in Task 12, re-exported here)
+export type { LlmProvider, LlmCompleteArgs, LlmCompleteResult } from './llm.js';
+
+// Template manifest validator (Task 6)
+export {
+  validateTemplateManifest,
+} from './template/manifest.js';
+export type {
+  TemplateManifest,
+  Section,
+  SectionKind,
+  Slot,
+  ValidationResult,
+} from './template/manifest.js';
